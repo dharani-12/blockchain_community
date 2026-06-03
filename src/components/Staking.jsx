ebad8fc90763e9bbc9c91988e05f77d93f2876d3
dharani-12 <p.dharani225@gmail.com>
2026-06-03 14:39:44 +0530

    changes

--- src/components/Staking.jsx
+++ src/components/Staking.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Button } from "@mui/material";
 import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
-import stakingImage from "../assets/staking.svg";
+import stakingImage from "../assets/Staking.svg";
 
 function Staking() {
   return (
